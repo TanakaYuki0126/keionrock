1de9d73e577f120a0d318bf1a6d0e065f5869c4a
田中　裕貴 <Tanaka@uBook-Air.local>
2021-02-27 16:22:36 +0900

    add start script

---
+++ scroll.js
@@ -0,0 +1,43 @@
+$(function () {
+    $('a[href^="#"]').click(function () {
+        let speed = 400;
+        let href = $(this).attr("href");
+        let target = $(href == "#" || href == "" ? 'html' : href);
+        let position = target.offset().top;
+        $('body,html').animate({ scrollTop: position }, speed, 'swing');
+        return false;
+    });
+});
+
+jQuery(window).on('scroll', function () {
+    if (jQuery('.main-message').height() * 2 < jQuery(this).scrollTop()) {
+        jQuery('.page-section').addClass('grayback');
+        jQuery('.slideshow').addClass('blur');
+
+    } else {
+        jQuery('.page-section').removeClass('grayback');
+        jQuery('.slideshow').removeClass('blur');
+    }
+});
+
+jQuery(window).on('scroll', function () {
+    if (jQuery('.main-message').height() * 6 < jQuery(this).scrollTop()) {
+        jQuery('.slideshow').addClass('blur');
+
+    } else {
+        jQuery('.slideshow').removeClass('blur');
+    }
+});
+
+$(document).ready(function () {
+    $(".navbar-nav li a").click(function (event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+    $(".navbar-brand").click(function (event) {
+        $(".navbar-collapse").collapse('hide');
+    });
+
+    $("p.question").on("click", function () {
+        $(this).next().slideToggle(200);
+    });
+});
