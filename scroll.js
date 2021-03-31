39183346e5421efee0d9dd6194b2202cc4687922
田中　裕貴 <Tanaka@uBook-Air.local>
2021-03-31 22:31:46 +0900

    add start script

--- scroll.js
+++ scroll.js
@@ -31,7 +31,7 @@ jQuery(window).on('scroll', function () {
 
 $(document).ready(function () {
     var heightSize = $(window).height();
-    $(".slideshow li").height(heightSize);
+    $(".slideshow li").height(heightSize * 1.3);
     $(".navbar-nav li a").click(function (event) {
         $(".navbar-collapse").collapse('hide');
     });
