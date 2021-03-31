1ca572f3a75c17c91c023b334071049ccec0bd13
田中　裕貴 <Tanaka@uBook-Air.local>
2021-03-31 22:30:22 +0900

    add start script

--- scroll.js
+++ scroll.js
@@ -30,6 +30,8 @@ jQuery(window).on('scroll', function () {
 });
 
 $(document).ready(function () {
+    var heightSize = $(window).height();
+    $(".slideshow li").height(heightSize);
     $(".navbar-nav li a").click(function (event) {
         $(".navbar-collapse").collapse('hide');
     });
@@ -40,4 +42,5 @@ $(document).ready(function () {
     $("p.question").on("click", function () {
         $(this).next().slideToggle(200);
     });
+
 });
